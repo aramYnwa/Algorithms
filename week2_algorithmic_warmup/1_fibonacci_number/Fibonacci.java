f13e6c7b66e8cd5c320705325fec84558ca6809c
Aramais Khachatryan <aramais.khachatryan@devolon.fi>
2020-04-16 18:58:22 +0300

    Fibonacci number

--- week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
+++ week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
@@ -8,10 +8,24 @@ public class Fibonacci {
     return calc_fib(n - 1) + calc_fib(n - 2);
   }
 
+  private static long calc_fib_fast(int n) { 
+    if (n <= 1)
+      return n;
+    
+    long[] numbers = new long[n+1];
+    numbers[0] = 0;
+    numbers[1] = 1;
+  
+    IntStream.range(2, n+1).forEach(
+      i -> numbers[i] = numbers[i-1] + numbers[i-2]
+    );
+    return numbers[n];
+  }
+
   public static void main(String args[]) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
 
-    System.out.println(calc_fib(n));
+    System.out.println(calc_fib_fast(n));
   }
 }
