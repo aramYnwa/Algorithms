1c072d0d356a53da26c9753d9d91036b926d0062
Aramais Khachatryan <rtgkid8@gmail.com>
2020-04-26 15:48:44 +0300

    Sum of Fibonaci numbers squares

--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/FibonacciSumSquares.java
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/FibonacciSumSquares.java
@@ -19,10 +19,27 @@ public class FibonacciSumSquares {
         return sum % 10;
     }
     
+    private static long getFibonacciSumSquaresFast(long n) {
+        if (n <= 1)
+            return  n;
+
+        int previous = 0;
+        int current  = 1;
+
+        for (int i = 0; i < n; ++i) {
+            int tmp_previous = previous % 10;
+            previous = current % 10;
+            current = (tmp_previous + current) % 10;
+        }
+
+        System.out.printf("res: %d", (previous * current));
+        return (previous * current) % 10;    
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumSquaresNaive(n);
+        long s = getFibonacciSumSquaresFast(n);
         System.out.println(s);
     }
 }
